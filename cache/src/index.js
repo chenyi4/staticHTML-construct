e5182cb0b8df24c013c5d0190f42abd15f4e1445
chenyi <chenyi4@qq.com>
2020-03-27 11:31:34 +0800

    静态页面js压缩

--- cache/src/index.js
+++ cache/src/index.js
@@ -10,7 +10,7 @@ async function getComponent() {
       element.innerHTML = _.join(['Hello', 'webpack'], ' ');
       element.appendChild(br);
       element.appendChild(button);
-
+    
     // Note that because a network request is involved, some indication
     // of loading would need to be shown in a production-level site/app.
     button.onclick = e => import(/* webpackChunkName: "math" */ './math').then(module => {
--- uglify压缩js/config/index.js
+++ uglify压缩js/config/index.js
@@ -2,21 +2,38 @@ var UglifyJS = require("uglify-es");
 var fs = require("fs");
 
 var conFig = {
-    enterFile: 'files/',
-    outFile: './dist'
+    enterFile: './../src/main/resources/static/js/',
+    outFile: 'files'
 }
 
+var fileNames = [
+  "cover.js",
+  "customize.js",
+  "customizeInit.js",
+  "customizeJqgridId.js",
+  "customizeSave.js",
+  "deviceDateTime.js",
+  "formcheck.js",
+  "jsplumbAttach.js",
+  "jsplumbCustomize.js",
+  "language.js",
+  "viewapilist.js"
+];
 
 deleteDist();
 
 var code = {}
-fs.readdirSync('files').map((file) => {
-    console.log(file);
-    var content = fs.readFile(conFig.enterFile+file,'utf-8', function (err,data) { 
-        code = {};
-        code[file] = data;
-        Uglify(code)
-     });
+fs.readdirSync(conFig.enterFile).map((file) => {
+  console.log(fileNames.indexOf(file));
+  if(fileNames.indexOf(file) >= 0){
+      if(file.indexOf('js') >= 0){
+          var content = fs.readFile(conFig.enterFile+file,'utf-8', function (err,data) { 
+              code = {};
+              code[file] = data;
+              Uglify(code)
+          });
+      }
+    }
 });
 
 function deleteDist(){
@@ -34,14 +51,16 @@ function deleteDist(){
 function Uglify(code) {
     var options = {
         mangle: {
-            toplevel: true,
+            toplevel: false, //不混淆全局的变量名
         },
+        unsafe_math: true,
         nameCache: {}
     };
     for(var codeI in code){
         var true_code = code[codeI];
         var result = UglifyJS.minify(true_code, options);
-        fs.writeFile(conFig.outFile+'/'+codeI, result.code,  function(err) {
+        var outFileName = codeI.replace('.js', '.min.js');
+        fs.writeFile(conFig.outFile+'/'+outFileName, result.code,  function(err) {
     
         });
     }
